30d7dd5050edd46cb95a64104f757dbe61578bc9
Robin Karlsson <rk222ev@student.lnu.se>
2015-09-12 15:41:02 +0200

    Created factory for user.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,17 @@
+require "faker"
+
 FactoryGirl.define do
-  factory :user do
-    
+  factory :user do |f|
+    password = Faker::Internet.password
+    f.username                  { Faker::Internet.user_name }
+    f.email                 { Faker::Internet.email }
+    f.password              { password }
+    f.password_confirmation { password }
+  end
+
+  factory :user_with_bad_confirmation, parent: :user do |f|
+    f.password  { "misspelledpassword" }
   end
 
 end
+
