b290b5f324616b00e3d5ddf7f17f59a1d4a6fbb7
Robin Karlsson <rk222ev@student.lnu.se>
2015-09-12 11:54:54 +0200

    Added translations related to the user.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -40,12 +40,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
    def configure_sign_up_params
-     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password) }
+     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
    end
 
   # If you have extra params to permit, append them to the sanitizer.
    def configure_account_update_params
-     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :current_password) }
+     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(
+                                                      :username, 
+                                                      :email, 
+                                                      :password, 
+                                                      :current_password, 
+                                                      :password_confirmation) }
    end
 
   # The path used after sign up.
