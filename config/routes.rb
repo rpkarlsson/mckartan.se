342bb5158a0d20eeb46051828289c620d1be7d05
Robin Karlsson <rk222ev@student.lnu.se>
2015-09-13 10:08:40 +0200

    Removed unused routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
- root to: 'pages#map'
-
-  # Pages
-  get     "about"       => "pages#about"
-
+  root to: 'pages#map'
   resources :sections, only: [:index, :show, :create, :destroy]
-
-  devise_for :users, controllers: { 
+  devise_for :users, controllers: {
                                   sessions: "users/sessions",
                                   registrations: "users/registrations"}
-
-  #get ':username', to: 'users#show', as: :user
-
 end
