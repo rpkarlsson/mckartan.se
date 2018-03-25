49af4a377c0b5a67e697a0df8bad99379166919b
Robin Karlsson <robinpkarlsson@gmail.com>
2018-03-25 13:52:13 +0200

    Fix layout

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -40,12 +40,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
    def configure_sign_up_params
-     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
+     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
    end
 
   # If you have extra params to permit, append them to the sanitizer.
    def configure_account_update_params
-     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(
+     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(
                                                       :username,
                                                       :email,
                                                       :password,
